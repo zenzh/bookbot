118c3fdd88cf4b3f41589469ac6d72285e6bead9
zenzh <czenzh@gmail.com>
2025-04-25 22:53:41 +0800

    finished project

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from stats import count_words, count_chars
+from stats import count_words, count_chars, sort_dict
+import sys
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
-    num_words = count_words(text)
-    print(f"{num_words} words found in the document")
-    print(count_chars(text))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
+    text = get_book_text(path)
+    character_count = count_chars(text)
+    word_count = count_words(text)
+
+    sort_dict(character_count, path, word_count)
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
--- stats.py
+++ stats.py
@@ -11,4 +11,25 @@ def count_chars(text):
         else:
             characters[character] += 1
     return characters
+
+def sort_on(dictionary):
+    return dictionary["num"]
+
+def sort_dict(dictionary, path, count):
+    list_of_characters = []
+    for key, value in dictionary.items():
+        list_of_characters.append({"char": key, "num": value})
+    list_of_characters.sort(reverse=True, key=sort_on)
     
+    print(f"""
+============ BOOKBOT ============
+Analyzing book found at {path}...
+----------- Word Count ----------
+Found {count} total words
+--------- Character Count -------""")
+
+    for character in list_of_characters:
+        if character["char"].isalpha():
+            print(f"{character["char"]}: {character["num"]}")
+
+    print("============= END ===============")
